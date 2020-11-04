db3ae4440dc40082403658fd64f015bd06da3a32
xwzwmt <18800206482@163.com>
2020-11-04 21:46:45 +0800

    week3 0.1

---
+++ Week_03/nio02/src/main/java/io/github/kimmking/gateway/outbound/myhttpclient/HttpClient.java
@@ -0,0 +1,115 @@
+package io.github.kimmking.gateway.outbound.myhttpclient;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.http.DefaultFullHttpResponse;
+import io.netty.handler.codec.http.FullHttpRequest;
+import io.netty.handler.codec.http.FullHttpResponse;
+import io.netty.handler.codec.http.HttpUtil;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+
+import static io.netty.handler.codec.http.HttpResponseStatus.NO_CONTENT;
+import static io.netty.handler.codec.http.HttpResponseStatus.OK;
+import static io.netty.handler.codec.http.HttpVersion.HTTP_1_1;
+
+public class HttpClient {
+
+    public  void doGetTestOne(FullHttpRequest fullRequest, ChannelHandlerContext ctx) {
+        // 获得Http客户端(可以理解为:你得先有一个浏览器;注意:实际上HttpClient与浏览器是不一样的)
+        CloseableHttpClient httpClient = HttpClientBuilder.create().build();
+        // 创建Get请求
+        HttpGet httpGet = new HttpGet("http://localhost:8808/"+fullRequest.uri());
+
+        // 响应模型
+        CloseableHttpResponse response = null;
+        try {
+            // 由客户端执行(发送)Get请求
+            response = httpClient.execute(httpGet);
+            // 从响应模型中获取响应实体
+            HttpEntity responseEntity = response.getEntity();
+            System.out.println("响应状态为:" + response.getStatusLine());
+            String s="";
+            if (responseEntity != null) {
+                 s=EntityUtils.toString(responseEntity);
+                System.out.println("响应内容长度为:" + responseEntity.getContentLength());
+                System.out.println("响应内容为:" + s);
+            }
+            handleResponse(fullRequest, ctx,s);
+        } catch (ClientProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                // 释放资源
+                if (httpClient != null) {
+                    httpClient.close();
+                }
+                if (response != null) {
+                    response.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void handleResponse(final FullHttpRequest fullRequest, final ChannelHandlerContext ctx, String endpointResponse) throws Exception {
+        FullHttpResponse response = null;
+        try {
+//            String value = "hello,kimmking";
+//            response = new DefaultFullHttpResponse(HTTP_1_1, OK, Unpooled.wrappedBuffer(value.getBytes("UTF-8")));
+//            response.headers().set("Content-Type", "application/json");
+//            response.headers().setInt("Content-Length", response.content().readableBytes());
+
+
+//            System.out.println(new String(body));
+//            System.out.println(body.length);
+//            String value=EntityUtils.toString(endpointResponse);
+            response = new DefaultFullHttpResponse(HTTP_1_1,OK,Unpooled.wrappedBuffer(endpointResponse.getBytes("UTF-8")));
+            response.headers().set("Content-Type", "application/json");
+//            response.headers().setInt("Content-Length", Integer.parseInt(String.valueOf(endpointResponse.getContentLength())));
+
+//            for (Header e : endpointResponse.getAllHeaders()) {
+//                //response.headers().set(e.getName(),e.getValue());
+//                System.out.println(e.getName() + " => " + e.getValue());
+//            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            response = new DefaultFullHttpResponse(HTTP_1_1, NO_CONTENT);
+            exceptionCaught(ctx, e);
+        } finally {
+            if (fullRequest != null) {
+                if (!HttpUtil.isKeepAlive(fullRequest)) {
+                    ctx.write(response).addListener(ChannelFutureListener.CLOSE);
+                } else {
+                    //response.headers().set(CONNECTION, KEEP_ALIVE);
+                    ctx.write(response);
+                }
+            }
+            ctx.flush();
+            //ctx.close();
+        }
+
+    }
+
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        cause.printStackTrace();
+        ctx.close();
+    }
+
+}
